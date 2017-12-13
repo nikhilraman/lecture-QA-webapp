ec604f890afbfe6337eea620f1e4028e9598b494
Nikhil Raman <ramann@seas.upenn.edu>
2017-12-13 02:40:17 -0500

    updating UI

--- routes/questions.js
+++ routes/questions.js
@@ -6,7 +6,8 @@ var qDB = require('../db/questions_ops.js');
 router.get('/home/:cid', function (req, res) { 
   if (req.session.isLoggedIn) {
     req.session.course = req.params.cid; 
-    res.render('home.ejs', {error: (req.query.error ? req.query.error : null)});
+    var split = req.params.cid.split('_');
+    res.render('home.ejs', {course: split[0], date: split[1], error: (req.query.error ? req.query.error : null)});
     // qDB.getQuestions(req.params.cid, function (err, data) {
     //   if (err) { 
     //     console.log('Error getting questions!');
